581ce98b2b0b3ad8ba766a792fdfe3764ac88c08
Sachit Rajbhandari <sachit.rajbhandari@utas.edu.au>
2020-02-25 09:19:38 +1100

    Publish GN3 artifacts to maven repo

---
+++ install-gn-release/install-gn-release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash    
+
+unzip -j "geonetwork.war" "META-INF/maven/*.properties" -d "geonetwork"
+
+groupId=`grep groupId geonetwork/pom.properties | cut -d'=' -f2`
+artifactId=`grep artifactId geonetwork/pom.properties | cut -d'=' -f2`
+version=`grep version geonetwork/pom.properties | cut -d'=' -f2`
+mavenUrl='s3://^${env.ARTIFACT_BUCKET}/repo/maven'
+
+mvn deploy:deploy-file -Dfile=geonetwork.war \
+ -DgroupId=^${groupId} -DartifactId=^${artifactId} -Dversion=^${version} -DgeneratePom=true \
+ -Durl=^${mavenUrl}
+
+included_artifacts="common core domain"
+for artifactId in \${included_artifacts}; do
+    filename="\${artifactId}-\${version}.jar"
+    unzip -j "geonetwork.war" "WEB-INF/lib/\${filename}" -d "geonetwork"
+    mvn deploy:deploy-file -Dfile="geonetwork/\${filename}" \
+     -DgroupId=\${groupId} -DartifactId=\${artifactId} -Dversion=\${version} -DgeneratePom=true \
+     -Durl=\${mavenUrl}
+done
+
