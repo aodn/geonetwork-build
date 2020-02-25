4f1d28c87603f1a360ca011a4def6c2906c1ef2c
Sachit Rajbhandari <sachit.rajbhandari@utas.edu.au>
2020-02-25 13:46:37 +1100

    Added README file and comments on install-gn-release script

---
+++ install-gn-release/README.md
@@ -0,0 +1,11 @@
+### Deploy a GeoNetwork War to Maven Repo
+
+Script to deploy GeoNetwork war and contained GeoNetwork artifacts to the IMOS maven repository.
+
+### Usage
+
+./install-gn-release.sh 
+
+### Note
+Make sure geonetwork.war file exists in the current directory.
+
--- install-gn-release/install-gn-release.sh
+++ install-gn-release/install-gn-release.sh
@@ -1,16 +1,18 @@
 #!/bin/bash    
 
+# Read groupId, artifactId and version from the pom.properties file
 unzip -j "geonetwork.war" "META-INF/maven/*.properties" -d "geonetwork"
-
 groupId=`grep groupId geonetwork/pom.properties | cut -d'=' -f2`
 artifactId=`grep artifactId geonetwork/pom.properties | cut -d'=' -f2`
 version=`grep version geonetwork/pom.properties | cut -d'=' -f2`
 mavenUrl='s3://${env.ARTIFACT_BUCKET}/repo/maven'
 
+# deploy war to s3 maven repo
 mvn deploy:deploy-file -Dfile=geonetwork.war \
  -DgroupId=${groupId} -DartifactId=${artifactId} -Dversion=${version} -DgeneratePom=true \
  -Durl=${mavenUrl}
 
+# deploy included artifacts to s3 maven repo
 included_artifacts="common core domain"
 for artifactId in ${included_artifacts}; do
     filename="${artifactId}-${version}.jar"
