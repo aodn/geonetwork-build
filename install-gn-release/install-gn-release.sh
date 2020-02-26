85b64bf417a083a7cd36bc5eab7f67584cfdd26c
Leigh Gordon <lwgordonimos@users.noreply.github.com>
2020-02-26 12:18:49 +1100

    Update install-gn-release.sh
    Move bucket name to bash script parameter

--- install-gn-release/install-gn-release.sh
+++ install-gn-release/install-gn-release.sh
@@ -1,13 +1,15 @@
-#!/bin/bash    
+#!/bin/bash
 
-set -ex
+set -euxo pipefail
+
+ARTIFACT_BUCKET=$1
 
 # Read groupId, artifactId and version from the pom.properties file
 unzip -j "geonetwork.war" "META-INF/maven/*.properties" -d "geonetwork"
 groupId=`grep groupId geonetwork/pom.properties | cut -d'=' -f2`
 artifactId=`grep artifactId geonetwork/pom.properties | cut -d'=' -f2`
 version=`grep version geonetwork/pom.properties | cut -d'=' -f2`
-mavenUrl='s3://${env.ARTIFACT_BUCKET}/repo/maven'
+mavenUrl="s3://${ARTIFACT_BUCKET}/repo/maven"
 
 # deploy war to s3 maven repo
 mvn deploy:deploy-file -Dfile=geonetwork.war \
@@ -23,4 +25,3 @@ for artifactId in ${included_artifacts}; do
      -DgroupId=${groupId} -DartifactId=${artifactId} -Dversion=${version} -DgeneratePom=true \
      -Durl=${mavenUrl}
 done
-
