2aa998503e35d22898441ba3b1fb10103de217d4
Sachit Rajbhandari <sachit.rajbhandari@utas.edu.au>
2020-02-26 14:31:39 +1100

    Publish maven gn3 (#17)
    Publish GN3 artifacts to maven repo

---
+++ install-gn-release/README.md
@@ -0,0 +1,11 @@
+### Deploy a GeoNetwork War to Maven Repo
+
+Script to deploy GeoNetwork war and contained GeoNetwork artifacts to the IMOS maven repository.
+
+### Usage
+
+./install-gn-release.sh <ARTIFACT_BUCKET>
+
+### Note
+Make sure geonetwork.war file exists in the current directory and provide artifact_bucket name as a parameter.
+
---
+++ install-gn-release/install-gn-release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+ARTIFACT_BUCKET=$1
+
+# Read groupId, artifactId and version from the pom.properties file
+unzip -j "geonetwork.war" "META-INF/maven/*.properties" -d "geonetwork"
+groupId=`grep groupId geonetwork/pom.properties | cut -d'=' -f2`
+artifactId=`grep artifactId geonetwork/pom.properties | cut -d'=' -f2`
+version=`grep version geonetwork/pom.properties | cut -d'=' -f2`
+mavenUrl="s3://${ARTIFACT_BUCKET}/repo/maven"
+
+# deploy war to s3 maven repo
+mvn deploy:deploy-file -Dfile=geonetwork.war \
+ -DgroupId=${groupId} -DartifactId=${artifactId} -Dversion=${version} -DgeneratePom=true \
+ -Durl=${mavenUrl}
+
+# deploy included artifacts to s3 maven repo
+included_artifacts="common core domain"
+for artifactId in ${included_artifacts}; do
+    filename="${artifactId}-${version}.jar"
+    unzip -j "geonetwork.war" "WEB-INF/lib/${filename}" -d "geonetwork"
+    mvn deploy:deploy-file -Dfile="geonetwork/${filename}" \
+     -DgroupId=${groupId} -DartifactId=${artifactId} -Dversion=${version} -DgeneratePom=true \
+     -Durl=${mavenUrl}
+done
