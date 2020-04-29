eb3b5e9a9fc2b42e5da90b556f2a32d2f8d2500d
Sachit Rajbhandari <sachit.rajbhandari@utas.edu.au>
2020-04-29 16:49:04 +1000

    Removed unused imports and version in pom file

--- extension/aodn-listeners/src/main/java/au/org/emii/listeners/history/AodnRecordGroupOwnerChangeListener.java
+++ extension/aodn-listeners/src/main/java/au/org/emii/listeners/history/AodnRecordGroupOwnerChangeListener.java
@@ -1,6 +1,5 @@
 package au.org.emii.listeners.history;
 
-import org.fao.geonet.domain.StatusValue;
 import org.fao.geonet.events.history.RecordGroupOwnerChangeEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
