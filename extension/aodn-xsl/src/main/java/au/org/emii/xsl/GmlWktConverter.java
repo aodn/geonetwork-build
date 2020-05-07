2e13fff9b6f42bac78a13cbb387391bfe6c95e8a
Craig Jones <Craig.Jones@utas.edu.au>
2020-05-07 12:36:12 +1000

    Index additional fields required by the portal And ensure the same dependency versions are used across plugins and match geonetwork versions where applicable

---
+++ extension/aodn-xsl/src/main/java/au/org/emii/xsl/GmlWktConverter.java
@@ -0,0 +1,87 @@
+package au.org.emii.xsl;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.io.WKTWriter;
+import org.apache.log4j.Logger;
+import org.fao.geonet.constants.Geonet;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.gml3.GMLConfiguration;
+import org.geotools.referencing.CRS;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.geotools.xml.Parser;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+
+import java.io.StringReader;
+
+
+/**
+ * Utility class for converting GML to WKT
+ *
+ * Handles both the old style of namespacing gml used by iso19139 and mcp (http://www.opengis.net/gml)
+ * and the new style of namespacing used by 19115-3 (http://www.opengis.net/gml/3.2) - better
+ * because it includes the version
+ *
+ * Both namespaces refer to gml 3.2 but require different parsers due to namespacing
+ */
+
+public class GmlWktConverter {
+    private static Logger logger = Logger.getLogger(GmlWktConverter.class);
+
+    private static final String GML32_NAMESPACE_URI = Geonet.Namespaces.GML32.getURI();
+
+    /**
+     * Returns a WKT representation of the passed GML string
+     *
+     * @param  gml a geometry encoded as gml
+     * @return wkt representation of the passed gml geometry in WGS 84
+     */
+    static public String gmlToWkt(String gml) {
+        try {
+            /* parse provide gml */
+            Parser parser = getParser(gml);
+            Geometry geom = (Geometry) parser.parse(new StringReader(gml));
+            /* convert to WGS84 if not already WGS84 */
+            Geometry wgs84Geom = toWgs84(geom);
+            /* 	write WKT */
+            WKTWriter writer = new WKTWriter();
+            return writer.write(wgs84Geom);
+        } catch (Exception e) {
+            logger.error("Could not parse " + gml + " " + e.getMessage());
+            return ""; // xsl friendly return value in the event of an error
+        }
+    }
+
+    /* Return required parser for gml based on presence of http://www.opengis.net/gml/3.2 namespace */
+
+    private static Parser getParser(String gml) {
+        Parser parser;
+        if (gml.contains(GML32_NAMESPACE_URI)) {
+            /* use a http://www.opengis.net/gml/3.2 parser */
+            parser = new Parser(new org.geotools.gml3.v3_2.GMLConfiguration());
+        } else {
+            /* use a http://www.opengis.net/gml parser */
+            parser = new Parser(new GMLConfiguration());
+        }
+        parser.setStrict(false);
+        parser.setValidating(false);
+        return parser;
+    }
+
+    /* Convert geometry to WGS 84 if not already WGS 84 */
+
+    static private Geometry toWgs84(Geometry geom) throws Exception {
+        // Return input geometry if CRS isn't available
+        if (!(geom.getUserData() instanceof CoordinateReferenceSystem)) return geom;
+
+        CoordinateReferenceSystem sourceCrs = (CoordinateReferenceSystem) geom.getUserData();
+
+        // Return the input geometry if input geometry's CRS is already WGS84
+        if (CRS.equalsIgnoreMetadata(sourceCrs, DefaultGeographicCRS.WGS84)) return geom;
+
+        // Otherwise return input geometry transformed to WGS84 CRS
+        MathTransform tform = CRS.findMathTransform(sourceCrs, DefaultGeographicCRS.WGS84);
+        return JTS.transform(geom, tform);
+    }
+
+}
--- main/src/main/webapp/WEB-INF/data/config/schema_plugins/iso19139/index-fields/md_metadata.xsl
+++ main/src/main/webapp/WEB-INF/data/config/schema_plugins/iso19139/index-fields/md_metadata.xsl
@@ -2,27 +2,34 @@
 <!-- Index a record for the main language -->
 <xsl:stylesheet xmlns:gmd="http://www.isotc211.org/2005/gmd"
                 xmlns:gco="http://www.isotc211.org/2005/gco"
-                xmlns:gml="http://www.opengis.net/gml/3.2"
-                xmlns:gml320="http://www.opengis.net/gml"
-                xmlns:srv="http://www.isotc211.org/2005/srv"
-                xmlns:geonet="http://www.fao.org/geonetwork"
                 xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
                 xmlns:gmx="http://www.isotc211.org/2005/gmx"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                xmlns:util="java:org.fao.geonet.util.XslUtil"
-                xmlns:skos="http://www.w3.org/2004/02/skos/core#"
-                xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                xmlns:converter="java:au.org.emii.xsl.GmlWktConverter"
+                xmlns:saxon="http://saxon.sf.net/"
                 version="2.0"
                 exclude-result-prefixes="#all">
 
-  <xsl:include href="default.xsl"/>
+    <xsl:include href="default.xsl"/>
 
-  <xsl:output method="xml" version="1.0" encoding="UTF-8" indent="no"/>
+    <xsl:output method="xml" version="1.0" encoding="UTF-8" indent="no"/>
 
-  <!-- Catch layer name and index them -->
-  <xsl:template mode="index"
-                match="/gmd:MD_Metadata/gmd:distributionInfo/gmd:MD_Distribution/gmd:transferOptions/gmd:MD_DigitalTransferOptions/gmd:onLine/gmd:CI_OnlineResource[gmd:linkage/gmd:URL!='' and not(contains(lower-case(gmd:linkage/gmd:URL), 'service=wms') and not(string(normalize-space(gmd:protocol/gco:CharacterString))))]">
-      <Field name="layer" string="{normalize-space(gmd:name/gco:CharacterString|gmd:name/gmx:MimeFileType)}" store="false" index="true"/>
-  </xsl:template>
+    <xsl:output name="serialisation-output-format" method="xml" omit-xml-declaration="yes"/>
+
+    <!-- Index layer name for use by aws-wps to lookup the metadata record for an aggregation -->
+    <xsl:template mode="index"
+                  match="gmd:transferOptions/*/gmd:onLine/gmd:CI_OnlineResource[gmd:protocol/*/text()='IMOS:NCWMS--proto' or contains(gmd:protocol, 'http-get-map')]">
+        <Field name="layer" string="{normalize-space(gmd:name/gco:CharacterString|gmd:name/gmx:MimeFileType)}" store="false" index="true"/>
+    </xsl:template>
+
+    <!-- Index bounding polygons as WKT for use by the portal to display the bounding polygons in the search results -->
+    <xsl:template mode="index" match="gmd:polygon">
+        <xsl:variable name="gml" select="saxon:serialize(., 'serialisation-output-format')"/>
+        <Field name="geoPolygon" string="{converter:gmlToWkt($gml)}" store="true" index="false"/>
+    </xsl:template>
+
+    <!-- Index point of truth link for use as an additional CSW queryable -->
+    <xsl:template mode="index" match="gmd:CI_OnlineResource[gmd:protocol/*/text()='WWW:LINK-1.0-http--metadata-URL']/gmd:linkage/gmd:URL">
+        <Field name="pointOfTruthLink" string="{text()}" store="true" index="false"/>
+    </xsl:template>
 
 </xsl:stylesheet>
